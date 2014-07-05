774187cc139bbb8241e57c45a13a2061fa378b9a
Justin Middleton <justin@whistle.com>
2014-07-04 18:11:24 -0700

    Add fluidsynth/mingus—we have sound

--- forgan.py
+++ forgan.py
@@ -3,12 +3,16 @@
 """
 Forgan, for use on a RasPI
 """
-
 import smbus
 import time
 from Adafruit_CAP1188 import Adafruit_CAP1188
+from mingus.midi import fluidsynth
 
 if __name__ == "__main__":
+
+    fluidsynth.init("/usr/share/sounds/sf2/FluidR3_GM.sf2", "alsa")
+
+    notes = [ 64 + (3 * i) for i in range(12) ]
 
     bus = smbus.SMBus(1)
     cap1_addr = 0x28
@@ -30,4 +34,6 @@ if __name__ == "__main__":
     cap2.write_register(Adafruit_CAP1188.STANDBYCFG, 0x30)
 
     while True:
-        print "touched: %s" % (cap1.touched + cap2.touched)
+        #print "touched: %s" % (cap1.touched + cap2.touched)
+        for note in (cap1.touched + cap2.touched):
+            fluidsynth.play_Note(note,0,127)
