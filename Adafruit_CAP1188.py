7fd32699e6ab19b51b6127c4eb3f1368bcbbd39c
Justin Middleton <justin@whistle.com>
2014-07-07 23:06:39 -0700

    * Move to interrupts rather than polling. * Add some more wrappers around register access.

--- Adafruit_CAP1188.py
+++ Adafruit_CAP1188.py
@@ -24,6 +24,19 @@ class Adafruit_CAP1188(object):
     Adafruit's Arduino library at https://github.com/adafruit/Adafruit_CAP1188_Library/.
     """
 
+    # Main Control Register
+    MAIN           =  0x00  # addr
+    MAIN_INT       =  0x01 << 0  # interrupt asserted
+    MAIN_DSLEEP    =  0x01 << 4  # deep sleep enabled
+    MAIN_STDBY     =  0x01 << 5  # standby enabled
+
+
+    # Sensor gain is encoded in 7-6 (bitwise "big endian")
+    MAIN_SENSGAIN1 =  0x01 << 6  # sensor gain 1
+    MAIN_SENSGAIN2 =  0x02 << 6  # sensor gain 2
+    MAIN_SENSGAIN4 =  0x04 << 6  # sensor gain 4
+    MAIN_SENSGAIN8 =  0x08 << 6  # sensor gain 8
+
     SENINPUTSTATUS =  0x03
     MTBLK          =  0x2A
     LEDLINK        =  0x72
@@ -31,9 +44,45 @@ class Adafruit_CAP1188(object):
     MANUID         =  0xFE
     STANDBYCFG     =  0x41
     REV            =  0xFF
-    MAIN           =  0x00
-    MAIN_INT       =  0x01
     LEDPOL         =  0x73
+
+    # Calibration Activation (Datasheet 5.11)
+    CAL_ACT_REG       = 0x26
+    CAL_ACT_CS1       = 0x01 << 0
+    CAL_ACT_CS2       = 0x01 << 1
+    CAL_ACT_CS3       = 0x01 << 2
+    CAL_ACT_CS4       = 0x01 << 3
+    CAL_ACT_CS5       = 0x01 << 4
+    CAL_ACT_CS6       = 0x01 << 5
+    CAL_ACT_CS7       = 0x01 << 6
+    CAL_ACT_CS8       = 0x01 << 7
+    CAL_ACT_ALL       = 0xff
+
+    # Repeat Rate Enable (Datasheet 5.13)
+    REPEAT_ENABLE_REG = 0x28
+    REPEAT_ENABLE_CS1 = 0x01 << 0
+    REPEAT_ENABLE_CS2 = 0x01 << 1
+    REPEAT_ENABLE_CS3 = 0x01 << 2
+    REPEAT_ENABLE_CS4 = 0x01 << 3
+    REPEAT_ENABLE_CS5 = 0x01 << 4
+    REPEAT_ENABLE_CS6 = 0x01 << 5
+    REPEAT_ENABLE_CS7 = 0x01 << 6
+    REPEAT_ENABLE_CS8 = 0x01 << 7
+    REPEAT_ENABLE_ALL = 0xFF
+    REPEAT_ENABLE_NONE = 0x00
+
+    # Configuration 2 (Datasheet 5.6.2)
+    CFG2_REG            = 0x44
+    CFG2_INT_REL_n      = 0x01 << 0 # setting this bit to 0 means press and release
+    CFG2_DIS_RF_NOISE   = 0x01 << 2 # disable RF noise filter
+    CFG2_SHOW_RF_NOISE  = 0x01 << 3 # show RF noise only; ignore EMI
+    CFG2_BLK_POL_MIR    = 0x01 << 4 # don't automatically set LED mirror as per polarity
+    CFG2_BLK_PWR_CTRL   = 0x01 << 5 # don't automatically power down cap sensor
+    CFG2_ALT_POL        = 0x01 << 6 # set ALERT# to active low / open drain
+    CFG2_INV_LINK_TRAN  = 0x01 << 7 # linked LED transition controls (sec 5.29)
+    CFG2_ENABLE_ALL     = 0xff
+
+
 
     def __init__(self, i2c_addr, i2c_bus, touch_offset = 0):
 
@@ -47,6 +96,16 @@ class Adafruit_CAP1188(object):
 
         self._i2c = I2CInfo(i2c_bus, i2c_addr)
         self._touch_offset = touch_offset
+
+    def __str__(self):
+        ret =  self.driver_name + "\n"
+        ret += "  mfg_id:       %s\n" % self.manufacturer_id
+        ret += "  product_id:   %s\n" % self.product_id
+        ret += "  revision:     %s\n" % self.revision
+        ret += "  multitouch:   %s\n" % self.multitouch_enabled
+        ret += "  leds_linked:  %s\n" % self.leds_linked
+        ret += "  touch_offset: %s\n" % self.touch_offset
+        return ret
 
     @property
     def is_i2c(self):
@@ -85,6 +144,89 @@ class Adafruit_CAP1188(object):
 
         if self.is_i2c:
             return self._i2c.read_byte_data(register)
+
+    def reset_interrupt(self):
+
+        """
+        This resets bit 0 of the MSR to clear asserted interrupt.
+        See datasheet section 5.1.
+        """
+
+        self.write_register(
+            Adafruit_CAP1188.MAIN,
+            self.read_register(Adafruit_CAP1188.MAIN)
+                & ~Adafruit_CAP1188.MAIN_INT
+        )
+        
+    @property
+    def driver_name(self):
+        return "Adafruit_CAP1188"
+
+    @property
+    def product_id(self):
+        return self.read_register(Adafruit_CAP1188.PRODID)
+
+    @property
+    def manufacturer_id(self):
+        return self.read_register(Adafruit_CAP1188.MANUID)
+
+    @property
+    def revision(self):
+        return self.read_register(Adafruit_CAP1188.REV)
+
+    @property
+    def touch_offset(self):
+        return self._touch_offset
+
+    @property
+    def repeat_enabled_status(self):
+        return self.read_register(Adafruit_CAP1188.REPEAT_ENABLE_REG)
+
+    @repeat_enabled_status.setter
+    def repeat_enabled_status(self, status):
+
+        """
+        Set the repeat enable (as per Datasheet section 5.13).
+        status will be bitwise-anded with REPEAT_ENABLE_ALL (0xFF).
+        """
+
+        self.write_register(
+            Adafruit_CAP1188.REPEAT_ENABLE_REG,
+            Adafruit_CAP1188.REPEAT_ENABLE_ALL & status
+        )
+
+    @property
+    def cfg2(self):
+
+        """
+        Get CFG2 registers (see datasheet sec 5.6.2)
+        """
+
+        return self.read_register(Adafruit_CAP1188.CFG2_REG)
+
+    @cfg2.setter
+    def cfg2(self, value):
+
+        """
+        Set CFG2 registers (see datasheet sec 5.6.2)
+        """
+
+        self.write_register(
+            Adafruit_CAP1188.CFG2_REG,
+            value & Adafruit_CAP1188.CFG2_ENABLE_ALL
+        )
+
+    def calibrate(self, pins = Adafruit_CAP1188.CAL_ACT_ALL):
+
+        """
+        Activate calibration for the given pins. Duration is 600ms.
+        See datasheet sec 5.11.
+        """
+
+        self.write_register(
+            Adafruit_CAPP1188.CAL_ACT_REG,
+            pins & Adafruit_CAP1188.CAL_ACT_ALL
+        )
 
     @property
     def multitouch_enabled(self):
@@ -131,6 +273,7 @@ class Adafruit_CAP1188(object):
             # TODO verify this
             self.write_register(Adafruit_CAP1188.LEDLINK, 0)
 
+
     @property
     def touched(self):
 
@@ -142,11 +285,7 @@ class Adafruit_CAP1188(object):
         touchval = self.read_register(Adafruit_CAP1188.SENINPUTSTATUS)
         ret = [i + self._touch_offset for i in range(0, 8) if touchval & 1<<i]
         if len(ret) > 0:
-            # reset touch
-            self.write_register(
-                Adafruit_CAP1188.MAIN,
-                self.read_register(Adafruit_CAP1188.MAIN) & ~Adafruit_CAP1188.MAIN_INT
-            )
+            self.reset_interrupt()
         return ret
 
 if __name__ == "__main__":
@@ -170,5 +309,5 @@ if __name__ == "__main__":
     cap1.write_register(Adafruit_CAP1188.STANDBYCFG, 0x30)
     cap2.write_register(Adafruit_CAP1188.STANDBYCFG, 0x30)
 
-    while True:
-        print "touched: %s" % cap1.touched + cap2.touched
+    print cap1
+    print cap2
--- forgan.py
+++ forgan.py
@@ -1,22 +1,51 @@
 #!/usr/bin/env python
 
 """
-Forgan, for use on a RasPI
+Forgan, for use on a RasPI.
 """
+
 import smbus
 import time
 from Adafruit_CAP1188 import Adafruit_CAP1188
 from mingus.midi import fluidsynth
+import RPIO
+import itertools
+
+class littlesynth(object):
+
+
+    def __init__(self, caps):
+        self.notes = [ 40 + (3 * i) for i in range(17) ] 
+        self.caps = caps
+
+    def play_note(self, note_index):
+        print "Playing note idx: %s" % note_index
+        fluidsynth.play_Note(self.notes[note_index], 0, 127)
+
+    def detect_touches(self, *args, **kwargs):
+        print "Interrupt fired; detecting touches."
+        print "args: %s", args
+        touchiter = (c.touched for c in self.caps)
+        for touchlist in touchiter:
+            [self.play_note(i) for i in touchlist]
 
 if __name__ == "__main__":
 
+    print "Initializing GPIO."
+
+    #GPIO.setmode(GPIO.BCM)
+
+    # for RPIO
+    RPIO.setmode(RPIO.BCM)
+
+    print "Initializing synth."
+
     fluidsynth.init("/usr/share/sounds/sf2/FluidR3_GM.sf2", "alsa")
 
-    notes = [ 64 + (3 * i) for i in range(12) ]
-
+    # Sensor Setup
     bus = smbus.SMBus(1)
-    cap1_addr = 0x28
-    cap2_addr = 0x29
+    cap1_addr = 0x29
+    cap2_addr = 0x28
 
     cap1 = Adafruit_CAP1188(cap1_addr, bus)
     cap2 = Adafruit_CAP1188(cap2_addr, bus, touch_offset = 8)
@@ -33,7 +62,60 @@ if __name__ == "__main__":
     cap1.write_register(Adafruit_CAP1188.STANDBYCFG, 0x30)
     cap2.write_register(Adafruit_CAP1188.STANDBYCFG, 0x30)
 
-    while True:
-        #print "touched: %s" % (cap1.touched + cap2.touched)
-        for note in (cap1.touched + cap2.touched):
-            fluidsynth.play_Note(note,0,127)
+    # Set interrupt event to press but not release
+    # and maintain open-drain, active low.
+    cfg2 = Adafruit_CAP1188.CFG2_INT_REL_n | Adafruit_CAP1188.CFG2_ALT_POL
+    cap1.cfg2 = cfg2
+    cap2.cfg2 = cfg2
+
+    # No repeat
+    cap1.repeat_enabled_status = Adafruit_CAP1188.REPEAT_ENABLE_NONE
+    cap2.repeat_enabled_status = Adafruit_CAP1188.REPEAT_ENABLE_NONE
+
+
+    # calibrate
+    # cap1.calibrate()
+    # cap2.calibrate()
+
+    # force enable irq
+    # cap1.write_register(0x27, 0xff)
+
+    synth = littlesynth([cap1, cap2])
+
+    TOUCH_INTERRUPT_PIN = 24
+    STOP_INTERRUPT_PIN  = 25
+
+    print "Setting up stop irq."
+
+    RPIO.setup(STOP_INTERRUPT_PIN, RPIO.IN, pull_up_down=RPIO.PUD_DOWN) 
+
+    print "Setting up synth irq."
+
+    RPIO.setup(TOUCH_INTERRUPT_PIN, RPIO.IN, pull_up_down=RPIO.PUD_UP) 
+
+    print "Adding event detection for touches."
+
+    RPIO.add_interrupt_callback(
+        TOUCH_INTERRUPT_PIN,
+        callback=synth.detect_touches,
+        edge="falling",
+        pull_up_down=RPIO.PUD_UP,
+        threaded_callback=False,
+        debounce_timeout_ms=0
+    )     
+
+    print "Adding event detection for reset."
+    RPIO.add_interrupt_callback(
+        STOP_INTERRUPT_PIN,
+        RPIO.stop_waiting_for_interrupts,
+        edge="rising",
+        pull_up_down=RPIO.PUD_DOWN,
+        threaded_callback=False,
+        debounce_timeout_ms=300
+    )     
+
+    # play until stop
+
+    RPIO.wait_for_interrupts()
+    print "Stopping!"
+    RPIO.cleanup()
